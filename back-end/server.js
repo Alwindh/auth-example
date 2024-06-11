8ec5abe1cbda96051426042ae8cde2959b984073
Alwin den Herder <42200913+Alwindh@users.noreply.github.com>
2024-06-11 18:07:24 +0200

    initial setup back-end

--- back-end/server.js
+++ back-end/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const cookieParser = require("cookie-parser");
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 const port = process.env.PORT || 4444;
 
 // Connect to MongoDB using Mongoose
